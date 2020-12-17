36539b5235604b8724381a922af2f0f1d7669f58
Trancever <dawidu@onet.pl>
2020-12-17 15:02:07 +0100

    feat: improve layout on web

--- example/src/Screen.tsx
+++ example/src/Screen.tsx
@@ -1,17 +1,39 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
 type Props = {
   children: React.ReactNode;
 };
 
+const width = Dimensions.get('window').width;
+
 export function Screen({ children }: Props) {
-  return <View style={styles.screen}>{children}</View>;
+  const [screenWidth, setScreenWidth] = React.useState(width);
+
+  React.useEffect(() => {
+    function onChange({ window }: { window: { width: number } }) {
+      setScreenWidth(window.width);
+    }
+
+    Dimensions.addEventListener('change', onChange);
+
+    return () => Dimensions.removeEventListener('change', onChange);
+  }, []);
+
+  return (
+    <View style={styles.screen}>
+      <View style={[styles.content, { width: screenWidth }]}>{children}</View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    alignItems: 'center',
+  },
+  content: {
     padding: 16,
+    maxWidth: 500,
   },
 });
